4aa641b44feb03bb35f621bfe80d650424c2343d
yudi <madewahyudi22@gmail.com>
2020-04-03 09:16:15 +0800

    Updates

---
+++ precache-manifest.53bbdd910a22bc741d276c0c055b1d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a617f4589aff97e3742552e185552f",
+    "url": "/pricing-component/index.html"
+  },
+  {
+    "revision": "b38bac8b56fdf3e677c8",
+    "url": "/pricing-component/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81cd7f9467fe9a64f95e",
+    "url": "/pricing-component/static/js/2.4fd6c835.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pricing-component/static/js/2.4fd6c835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38bac8b56fdf3e677c8",
+    "url": "/pricing-component/static/js/main.0e1a0dde.chunk.js"
+  },
+  {
+    "revision": "08674733150bd412edbe",
+    "url": "/pricing-component/static/js/runtime-main.8a13584c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pricing-component/static/media/logo.5d5d9eef.svg"
+  }
+]);
