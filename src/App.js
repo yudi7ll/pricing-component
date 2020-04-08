f800d8ec1d764102f073c1e1aa3ace45dfed37f3
yudi <madewahyudi22@gmail.com>
2020-04-08 13:24:03 +0800

    switch btn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 class="pricing">Our Pricing</h1>
+      <h1>Our Pricing</h1>
+      <div className="switch">
+        <h5 className="switch__text">Annually</h5>
+        <label className="switch__box">
+          <input type="checkbox" />
+          <span className="switch__slider"></span>
+        </label>
+        <h5 className="switch__text">Monthly</h5>
+      </div>
     </>
   );
 }
