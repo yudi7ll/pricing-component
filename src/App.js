b2812b5b912d449c986009989f2e7c65c44f127a
yudi <madewahyudi22@gmail.com>
2020-04-09 09:42:07 +0800

    reusable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
 import React from 'react';
 import './App.css';
 
+function PricingComponent({ title, price, features = [] }) {
+  const Features = () => features.map(feature => (
+    <li>{ feature }</li>
+  ));
+  
+  return (
+    <div className="pricing">
+      <div className="pricing__box pricing__basic">
+        <h3>{ title }</h3>
+        <h2 className="pricing__price">
+          $<span>{ price }</span>
+        </h2>
+        <ul className="pricing__offers">
+          <Features />
+        </ul>
+        <div className="pricing__btn">
+          <button type="button" onClick={ () => document.location.reload() }>LEARN MORE</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
+  const data = [
+    {
+      title: 'Basic',
+      price: '19.99',
+      features: [
+        '500 GB Storage',
+        '2 Users Allowed',
+        'Send up to 3 GB'
+      ]
+    }, {
+      title: 'Professional',
+      price: '24.99',
+      features: [
+        '1 TB Storage',
+        '5 Users Allowed',
+        'Send up to 10 GB'
+      ]
+    }, {
+      title: 'Master',
+      price: '39.99',
+      features: [
+        '2 TB Storage',
+        '10 Users Allowed',
+        'Send up to 20 GB'
+      ]
+    }
+  ];
+
+  const Pricing = () => data.map(({ title, price, features }) => (
+    <PricingComponent
+      title={title}
+      price={price}
+      features={features}
+    />
+  ));
+
   return (
     <>
       <h1>Our Pricing</h1>
@@ -13,57 +72,7 @@ function App() {
         </label>
         <h4 className="switch__text">Monthly</h4>
       </div>
-
-      <div className="pricing">
-        <div className="pricing__box pricing__basic">
-          <h3>Basic</h3>
-          <h2 className="pricing__price">
-            $<span>19.99</span>
-          </h2>
-          <ul className="pricing__offers">
-            <li>500 GB Storage</li>
-            <li>2 Users Allowed</li>
-            <li>Send up to 3 GB</li>
-          </ul>
-          <div className="pricing__btn">
-            <button type="button" onClick={ () => document.location.reload() }>LEARN MORE</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="pricing">
-        <div className="pricing__box pricing__box--blue">
-          <h3>Professional</h3>
-          <h2 className="pricing__price">
-            $<span>24.99</span>
-          </h2>
-          <ul className="pricing__offers">
-            <li>1 TB Storage</li>
-            <li>5 Users Allowed</li>
-            <li>Send up to 10 GB</li>
-          </ul>
-          <div className="pricing__btn pricing__btn--white">
-            <button type="button" onClick={ () => document.location.reload() }>LEARN MORE</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="pricing">
-        <div className="pricing__box pricing__basic">
-          <h3>Master</h3>
-          <h2 className="pricing__price">
-            $<span>39.99</span>
-          </h2>
-          <ul className="pricing__offers">
-            <li>2 TB Storage</li>
-            <li>10 Users Allowed</li>
-            <li>Send up to 20 GB</li>
-          </ul>
-          <div className="pricing__btn">
-            <button type="button" onClick={ () => document.location.reload() }>LEARN MORE</button>
-          </div>
-        </div>
-      </div>
+      <Pricing />
     </>
   );
 }
